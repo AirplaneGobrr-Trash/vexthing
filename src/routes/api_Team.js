638827642d21261438c458684399f7c9e5839425
AirplaneGoBrr <mottlcompound@gmail.com>
2024-02-01 10:42:38 -0800

    Get batch team data + highlight winning team

--- src/routes/api_Team.js
+++ src/routes/api_Team.js
@@ -4,45 +4,90 @@ const utils = require("../helpers/utils")
 const multer = require("multer")
 
 router.get("/events/:teamID", async (req, res) => {
-    let teamID = req.params.teamID
+  let teamID = req.params.teamID
 
-    const team = await rApi.team(teamID)
-    const events = await team.getEvents()
+  const team = await rApi.team(teamID)
+  const events = await team.getEvents()
 
-    res.send(events)
+  res.send(events)
 })
 
 // TODO: Make this a "BATCH" to prevent sending so many requests
 router.get("/data/:eventID/:teamID", async (req, res) => {
-    let eventID = req.params.eventID
-    let teamID = req.params.teamID
-    let imgMode = req.query.picture ?? teamID.includes(".png")
-    teamID = teamID.split(".png").shift()
-    console.log("upload", teamID)
+  let eventID = req.params.eventID
+  let teamID = req.params.teamID
+  let imgMode = req.query.picture ?? teamID.includes(".png")
+  teamID = teamID.split(".png").shift()
+  console.log("upload", teamID)
 
-    let eventData = await rApi.event(eventID).getData()
+  let eventData = await rApi.event(eventID).getData()
 
-    if (!eventData) return res.status(404).send(`No event found with ID ${eventID}`)
+  if (!eventData) return res.status(404).send(`No event found with ID ${eventID}`)
+  let team = await rApi.team(teamID)
+  let teamData = team.getData()
+  if (!teamData) return res.status(404).send(`No team found with ID ${teamID}`)
+
+  let eventStoredData = await utils.getEventData(eventID)
+  let teamStoredData = await eventStoredData.getTeamData(teamID)
+  if (imgMode) {
+    let img = teamStoredData?.picture
+    if (!img) return res.send("No Picture!")
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': img.length
+    });
+    res.end(img);
+    return
+  }
+  if (teamStoredData) teamStoredData.picture = `http://${req.headers.host}${req.originalUrl}?picture=true`
+  res.send(teamStoredData ?? {})
+})
+
+router.post("/data/:eventID", async (req, res) => {
+  let eventID = req.params.eventID
+
+  let data = req.body
+  let teamIDs = data.teamIDs
+  console.log(data, teamIDs)
+
+  let eventData = await rApi.event(eventID).getData()
+  if (!eventData) return res.status(404).send(`No event found with ID ${eventID}`)
+
+  let back = {}
+  for (let teamID of teamIDs) {
     let team = await rApi.team(teamID)
     let teamData = team.getData()
-    if (!teamData) return res.status(404).send(`No team found with ID ${teamID}`)
+    if (!teamData) {
+      back[teamID] = null
+      continue
+    }
 
     let eventStoredData = await utils.getEventData(eventID)
     let teamStoredData = await eventStoredData.getTeamData(teamID)
-    if (imgMode) {
-        let img = teamStoredData?.picture
-        if (!img) return res.send("No Picture!")
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': img.length
-        });
-        res.end(img);
-        return
-    }
-    if (teamStoredData) teamStoredData.picture = `http://${req.headers.host}${req.originalUrl}?picture=true`
-    res.send(teamStoredData ?? {})
-})
+    back[teamID] = teamStoredData
+  }
+  return res.send(back)
 
+  let teamID = req.params.teamID
+  let imgMode = req.query.picture ?? teamID.includes(".png")
+  teamID = teamID.split(".png").shift()
+  console.log("upload", teamID)
+
+  let eventStoredData = await utils.getEventData(eventID)
+  let teamStoredData = await eventStoredData.getTeamData(teamID)
+  if (imgMode) {
+    let img = teamStoredData?.picture
+    if (!img) return res.send("No Picture!")
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': img.length
+    });
+    res.end(img);
+    return
+  }
+  if (teamStoredData) teamStoredData.picture = `http://${req.headers.host}${req.originalUrl}?picture=true`
+  res.send(teamStoredData ?? {})
+})
 
 const upload = multer()
 
